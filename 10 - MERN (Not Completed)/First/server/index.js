60ad0943493b07e9e9d462c159cb3eff493cd944
ayman670 <aymansaud67@gmail.com>
2022-09-26 17:27:36 -0700

    MERN fix some problems

--- 10 - MERN (Not Completed)/First/server/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config(); // .env file
-
-const app = require('./src/app');
-
-const PORT = process.env.PORT || 8080
-
-app.listen(PORT,()=>{
-    console.log(`server is ready for connections on port ${PORT}`);
-})
